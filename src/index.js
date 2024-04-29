9ada60ddf74ede3cabe38ecf3c001d81091d5164
Haydê Carlos <86674944+haydecarlos@users.noreply.github.com>
2024-04-29 14:18:42 -0300

    gradient circle

--- src/index.js
+++ src/index.js
@@ -2,13 +2,37 @@ import p5 from "p5";
 
 /** @param {p5} p */
 const sketch = (p) => {
+  let ctx;
+
   p.setup = () => {
     p.createCanvas(p.windowWidth, p.windowHeight);
+    
+    ctx = p.drawingContext;
+
   };
 
   p.draw = () => {
-    p.background(50);
-    p.ellipse(p.width / 2, p.height / 2, 200, 200);
+    p.background(100);
+
+    const grad = ctx.createRadialGradient(
+      p.width/ 3,
+      p.height/2, 
+      1, 
+      p.width/3, 
+      p.height/2, 
+      p.width/3,
+    );
+    grad.addColorStop(0, "rgba(8, 17, 51, 100)");
+    grad.addColorStop(0.6,"rgba(0, 55, 132, 100)");
+    grad.addColorStop(1, "rgba(229, 232, 226, 0)");
+
+    p.push();
+    p.noStroke();
+    ctx.fillStyle= grad;
+  
+    p.rect(0, 0, p.width, p.height);
+    p.pop();
+
   };
 
   p.windowResized = () => {
